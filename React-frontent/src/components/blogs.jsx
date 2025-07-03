1a76252a5c9d9a45cf1c956efdf0211806a0209c
Aparnamol K S <aparnamolks.off@gmail.com>
2025-07-04 02:16:16 +0530

    adding files

---
+++ React-frontent/src/components/blogs.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+
+function Blogs() {
+    const navigate = useNavigate();  
+    function createBlog(){
+        window.location = '/createBlog'
+    }
+
+    function view(id){
+        navigate(`/blogs/${id}`); 
+    }
+
+
+    const [blogs, setBlogs] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:4000/blogs', {
+            headers: {
+                token: localStorage.getItem('token')
+            }
+        }).then(function (response) {
+            console.log(response.data)
+
+            setBlogs(response.data)
+        })
+    }, []);
+
+    return <div style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        fontFamily: "cambria"
+    }}>
+        <h1 style={{fontSize:"60px"}}>Blogs</h1>
+        <button style={{backgroundColor:"black", color:"white", borderRadius:"5px", padding:"5px",width:"100px"}} onClick={createBlog}>Create Blog</button>
+        {blogs.map(blog=>
+             <div key={blog._id}  style={{padding:"10px", border:"1px solid black", width:"50%",boxShadow:"3px 2px 2px black",margin:"10px",cursor:"pointer"}} onClick={()=>view(blog._id)}>
+                <h3>{blog.title}</h3>
+                <p>{blog.content}</p>
+            </div>
+        )}
+
+    </div>
+}
+
+
+
+export default Blogs;
+
